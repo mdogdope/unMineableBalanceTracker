85c05a06ba01c91074ea1a92fe44239a0108ae00
Michael Heston <m45t3rhest@gmail.com>
2022-02-23 13:37:45 -0700

    INIT

---
+++ src/com/github/mdogdope/unmineablebalancetracker/Config.java
@@ -0,0 +1,5 @@
+package com.github.mdogdope.unmineablebalancetracker;
+
+public class Config {
+
+}
---
+++ src/com/github/mdogdope/unmineablebalancetracker/Main.java
@@ -0,0 +1,9 @@
+package com.github.mdogdope.unmineablebalancetracker;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
